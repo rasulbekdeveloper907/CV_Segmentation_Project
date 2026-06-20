8996b083fe6d83b5365010f48fac520b55a964e0
Rasulbek <rassiazzi9218@gmail.com>
2026-06-20 07:49:01 +0500

    fsdf

--- api/main.py
+++ api/main.py
@@ -1,88 +1,62 @@
+from fastapi import FastAPI, File, UploadFile
 from ultralytics import YOLO
-import gradio as gr
-import cv2
 import numpy as np
+import cv2
+from PIL import Image
+import io
 
-# Model yuklash
+app = FastAPI(title="Road Crack Detection API")
+
+# Model load
 model = YOLO("../models/best.pt")
 
 
-def detect_crack(image):
+@app.get("/")
+def home():
+    return {"message": "Road Crack Detection API is running 🚀"}
 
-    results = model.predict(
-        source=image,
-        conf=0.25,
-        verbose=False
-    )
 
-    result = results[0]
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
 
-    output = image.copy()
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data)).convert("RGB")
+    image = np.array(image)
 
+    results = model.predict(image, conf=0.25, verbose=False)[0]
+
+    detections = []
     crack_count = 0
     total_area = 0
 
-    if result.masks is not None:
+    if results.masks is not None:
 
-        masks = result.masks.data.cpu().numpy()
-
+        masks = results.masks.data.cpu().numpy()
         crack_count = len(masks)
 
-        for mask in masks:
+        for i, mask in enumerate(masks):
 
-            mask = cv2.resize(
-                mask,
-                (image.shape[1], image.shape[0])
-            )
+            mask = cv2.resize(mask, (image.shape[1], image.shape[0]))
+            mask = (mask > 0.5).astype(np.uint8)
 
             area = int(mask.sum())
-
             total_area += area
 
-            colored_mask = np.zeros_like(image)
+            detections.append({
+                "id": i,
+                "area": area
+            })
 
-            colored_mask[:, :, 0] = (
-                mask * 255
-            ).astype(np.uint8)
-
-            output = cv2.addWeighted(
-                output,
-                1,
-                colored_mask,
-                0.4,
-                0
-            )
-
-    severity = "Low"
-
-    if total_area > 5000:
+    if total_area < 5000:
+        severity = "Low"
+    elif total_area < 15000:
         severity = "Medium"
-
-    if total_area > 15000:
+    else:
         severity = "High"
 
-    report = f"""
-Road Crack Analysis
-
-Crack Count: {crack_count}
-
-Crack Area: {total_area} pixels
-
-Severity: {severity}
-"""
-
-    return output, report
-
-
-app = gr.Interface(
-    fn=detect_crack,
-    inputs=gr.Image(type="numpy"),
-    outputs=[
-        gr.Image(label="Segmentation Result"),
-        gr.Textbox(label="Report")
-    ],
-    title="Road Crack Detection",
-    description="YOLOv11 Segmentation + Gradio"
-)
-
-app.launch()
+    return {
+        "crack_count": crack_count,
+        "total_area": total_area,
+        "severity": severity,
+        "detections": detections
+    }
